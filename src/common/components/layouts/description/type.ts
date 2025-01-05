879f17e22b8902d079b15c332ccb4806d3f6bc5f
55555-Jyeon <jyeon380516@gmail.com>
2025-01-05 23:45:12 +0900

    🐞 fix: conflict(with company commit)

--- src/common/components/layouts/description/type.ts
+++
@@ -1,3 +0,0 @@
-export type pageDescriptionProps = {
-  sources: string[];
-};
---
+++ src/common/layouts/description/page.tsx
@@ -0,0 +1,34 @@
+import desc from "./style.module.scss";
+import { type pageDescriptionProps } from "./type";
+import { BasicIcons } from "@/assets/icons";
+
+const PageDescription = ({ sources }: pageDescriptionProps): JSX.Element => {
+    return (
+        <div className={desc.container}>
+            <div className={desc.srcWrapper}>
+                {sources.map((source, index) => (
+                    <div key={source} className={desc.src}>
+                        <p
+                            style={{
+                                color:
+                                    index === sources.length - 1
+                                        ? "black"
+                                        : "rgba(0, 0, 0, 0.4)",
+                            }}
+                        >
+                            {source}
+                        </p>
+                        {index < sources.length - 1 && (
+                            <img src={BasicIcons.arrow.right} alt="chevron" />
+                        )}
+                    </div>
+                ))}
+            </div>
+            <div className={desc.desc}>
+                파라미터를 설정하여 측정한 신호를 진단에 적절한 형태로 변환시킬
+                수 있습니다.
+            </div>
+        </div>
+    );
+};
+export default PageDescription;
