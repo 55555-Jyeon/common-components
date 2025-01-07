071c9523c07e0af1998f0207c82a0c9566bec441
55555-Jyeon <jyeon380516@gmail.com>
2025-01-07 23:38:43 +0900

    📂 docs : fix few rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,31 +8,31 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:prettier/recommended",
     ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ["*.ts", "*.tsx"],
-            parserOptions: {
-                sourceType: "script",
-            },
-            rules: {
-                "@typescript-eslint/no-var-requires": "off",
-            },
-        },
-    ],
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        extraFileExtensions: [".scss"],
+        extraFileExtensions: [".scss", ".css"], // SCSS 및 CSS 확장자 추가
         project: "./tsconfig.json",
     },
-    plugins: ["react"],
+    plugins: ["react", "import"], // import 플러그인 추가
     rules: {
         "react/react-in-jsx-scope": "off", // React 17 이상에서는 필요 없음
         "prettier/prettier": ["error", { endOfLine: "auto" }], // Prettier와 줄바꿈 충돌 방지
         "@typescript-eslint/consistent-type-definitions": "off",
+        "import/extensions": [
+            // 확장자 처리
+            "error",
+            "always",
+            {
+                js: "never",
+                jsx: "never",
+                ts: "never",
+                tsx: "never",
+                scss: "always",
+                css: "always",
+            },
+        ],
+        "import/no-unresolved": "error", // 파일이 제대로 해석되지 않으면 오류 발생
     },
     settings: {
         react: {
