1f6980c0e9c1810fcacf26d090b4defd6650cad7
55555-Jyeon <jyeon380516@gmail.com>
2025-01-11 19:26:07 +0900

    🐞 fix: fix lint error(stylelint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,19 +9,18 @@ module.exports = {
         "plugin:prettier/recommended",
         "plugin:import/typescript",
     ],
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        extraFileExtensions: [".scss", ".css"], // SCSS 및 CSS 확장자 추가
         project: "./tsconfig.json",
     },
     plugins: ["react", "import"],
     rules: {
-        "react/react-in-jsx-scope": "off", // React 17 이상에서는 필요 없음
-        "prettier/prettier": ["error", { endOfLine: "auto" }], // Prettier와 줄바꿈 충돌 방지
+        "react/react-in-jsx-scope": "off",
+        "prettier/prettier": ["error", { endOfLine: "auto" }],
         "@typescript-eslint/consistent-type-definitions": "off",
         "import/extensions": [
-            // 확장자 처리
             "error",
             "ignorePackages",
             {
@@ -33,11 +32,17 @@ module.exports = {
                 css: "always",
             },
         ],
-        "import/no-unresolved": "error", // 파일이 제대로 해석되지 않으면 오류 발생
+        "import/no-unresolved": "error",
     },
     settings: {
         react: {
-            version: "detect", // React 버전을 자동으로 감지
+            version: "detect",
+        },
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
         },
     },
+    ignorePatterns: ["*.scss", "*.css", ".eslintrc.js"],
 };
