db524e78afbcc6581c6140c30bb8eb565724d096
55555-Jyeon <jyeon380516@gmail.com>
2025-01-11 19:25:17 +0900

    🟢 feat: create footer and layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Layout from "./common/layouts";
+import Home from "./pages/Home";
 import "./App.css";
-import { Header } from "./common/layouts/header";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <Header />
-            hello, world
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
