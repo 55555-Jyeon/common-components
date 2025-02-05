ea5c6139ed9ac99f1bc51c009d13c50d647c8599
55555-Jyeon <jyeon380516@gmail.com>
2025-02-05 22:44:22 +0900

    🟢 feat: revive page description component

--- src/common/layouts/index.tsx
+++ src/common/layouts/index.tsx
@@ -3,6 +3,7 @@ import Header from "./header";
 import Footer from "./footer";
 import SideNavbar from "./navigation/page";
 import layout from "./style.module.scss";
+import PageDescription from "./description/page";
 
 const Layout = (): JSX.Element => {
     return (
@@ -11,6 +12,10 @@ const Layout = (): JSX.Element => {
             <main className={layout.contents}>
                 <SideNavbar />
                 <div className={layout.outlet}>
+                    <PageDescription
+                        sources={["one depth", "two depth", "three depth"]}
+                        desc="페이지에 대한 설명을 적을 수 있습니다."
+                    />
                     <Outlet />
                 </div>
             </main>
